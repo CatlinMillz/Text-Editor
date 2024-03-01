339f67171f3391344ce64aab67673e8e376ff2e7
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2024-03-01 14:15:02 -0500

    fixed missing files re-edited code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,17 +4,15 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: 'development',
-    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js'
+      install: './src/js/install.js'
     },
-    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
@@ -37,8 +35,8 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'Text Editor',
-        short_name: 'J.A.T.E.',
-        description: 'Runs in the browser!',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
@@ -51,23 +49,6 @@ module.exports = () => {
           },
         ],
       }),
-    ],
-
-// TODO: Add CSS loaders and babel to webpack.
-
-
-module: {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
     ],
 
     module: {
@@ -90,6 +71,5 @@ module: {
         },
       ],
     },
-  },
-}
+  };
 };
